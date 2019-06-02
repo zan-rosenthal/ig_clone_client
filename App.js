3b8b6e229d37c7fb9af927aa79ce1f1cc74f0866
zan-rosenthal <zan.rosenthal@gmail.com>
2019-06-02 12:19:33 -0500

    Add navigation

--- App.js
+++ App.js
@@ -12,26 +12,5 @@ const MainNavigator = createStackNavigator({
   initialRouteName: 'Profile'
 });
 
-const Router = createAppContainer(MainNavigator);
+export default createAppContainer(MainNavigator);
 
-// export default App;
-
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Router />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
